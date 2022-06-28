21b399ed07fd1e3fb976d10e402a7fb6ee5152f1
Muhammad Arsalan <muhammadarsalan@bngs-MacBook-Pro.local>
2022-06-28 13:02:00 -0700

    add business logic befor db

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      // do some thing
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+  @Post()
+  createTask(@Body() creteTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(creteTaskDto);
+  }
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+  @Patch('/:id/status') // we are not use dto when we get data from params
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTask(id, status);
+  }
+}
